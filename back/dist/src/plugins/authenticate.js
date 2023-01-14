2ef925ae740dec18fe1b9cdd1fc2475bb32c181b
Carlos Vinicius <carlos09v@gmail.com>
2023-01-14 09:36:50 -0300

    1.0.5
    heroku is not free anymore and tryng to use PlanetScale
    
    em busca de um futuro melhor :)))
    
    Create environment.d.ts
    
    Update server.ts
    
    uma hora vai.....
    
    era uma vez...
    
    pure com batata
    
    Ziguiriguidum Ziguiriguidum
    
    Update server.ts
    
    Update README.md
    
    fixing readme
    
    Update package.json
    
    Delete vercel.json
    
    Update server.ts
    
    trying to deploy using netlify

--- back/dist/src/plugins/authenticate.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authenticate = void 0;
-const authenticate = async (req) => {
-    await req.jwtVerify();
-};
-exports.authenticate = authenticate;
